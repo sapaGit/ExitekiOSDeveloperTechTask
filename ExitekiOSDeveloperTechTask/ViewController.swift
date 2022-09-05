5240164990880931db8ce331fa6853123f9fd516
Sergey Pavlov <mosevaly@gmail.com>
2022-09-05 19:15:11 +0300

    Initial Commit

---
+++ ExitekiOSDeveloperTechTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExitekiOSDeveloperTechTask
+//
+//  Created by Sergey Pavlov on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
